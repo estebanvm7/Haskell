7aacedf47aaacb6948cbb0d9bdd4a78fd812092a
Mauricio-Arce <arce162011@gmail.com>
2015-05-19 08:58:32 -0600

    Update stopwords.hs
    Optimizacion de codigo

--- stopwords.hs
+++ stopwords.hs
@@ -3,18 +3,13 @@ import System.Environment
 main = do
 	args <- getArgs
 	valoresDigitados args
-	
-archivoPalabras [fileName] elemento = do
-    contenido <- readFile "Stopwords.txt"
-    let lineas = lines contenido
-        listaPalabras = zipWith (\n line -> line) [0..] lineas
-    resultado listaPalabras elemento 
 
-valoresDigitados [] = return () 
+valoresDigitados [] = return ()
 valoresDigitados (head:tail) = do
-	archivoPalabras ["Stopwords.txt"] head 
+	contenido <- readFile "Stopwords.txt"
+	resultado (lines contenido) head
 	valoresDigitados tail
 
-resultado [] palabra = return ()
+resultado [] palabra =  print palabra
 resultado (head:tail) palabra = do
-	if  head /= palabra then resultado tail palabra else print palabra
+	if head /= palabra then resultado tail palabra else return ()
