f105f3b280586f672dd0ecffda0833640a424865
Mauricio-Arce <arce162011@gmail.com>
2015-05-27 11:33:45 -0600

    Update buscador.hs
    Cambio de la ruta del indice(a como lo solicitaba el documento de la proga) y cambio del nombre de la función ( de join a buscar) porque aveces tiraba un warning(en la ejecución) debido a que join es una palabra reservada de haskell para Monads(es una funcion)

--- Progra/buscador.hs
+++ Progra/buscador.hs
@@ -5,14 +5,14 @@ import Control.Monad ( mapM_)
 import System.Environment ( getArgs )
 import Trie
 
-join :: [String] -> Trie -> [String]
-join [a] trie = (path (searchNode trie a))
-join (head : tail) trie = intersect (path (searchNode trie head)) (join tail trie) 
+buscar :: [String] -> Trie -> [String]
+buscar [a] trie = (path (searchNode trie a))
+buscar (head : tail) trie = intersect (path (searchNode trie head)) (buscar tail trie) 
 
 main = do
   arg <- getArgs
-  input <- readFile "trie.txt"
+  input <- readFile "/home/.indice"
   let trie = read input :: [Node]
-  let node = join arg trie
+  let node = buscar arg trie
   mapM_ putStrLn node
 
